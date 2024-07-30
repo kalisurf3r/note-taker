6f59a87d89f1cd3cf88b9da7288ea17ad59bf257
kalisurf3r <ayalaarturo925@gmail.com>
2024-07-29 19:17:54 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # note-taker
+
+---
+
+### Welcome to my note-taker repo
+
+- free and easy to use
+- works across multiple devices and saves notes
+- write, read, delete
+
+### Checkout Render link for full app
+
+---
--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
